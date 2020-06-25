9b16111f216df6de7449fe81b55603f171ddc33f
shuva <shuvodev500@gmail.com>
2020-06-25 09:41:26 +0600

    First Commit

---
+++ admin/aboutus.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>About</title>
+<link rel="stylesheet" href="../csss/aboutus.css" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+
+</head>
+<body>
+    <header>
+      <nav>
+        <div class="row clearfix">
+          
+            <ul class="main-nav" animate slideInDown>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="contactus.php">Contact Us</a></li>
+                <li><a href="../login.php">Admin Login</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="main-content-header">
+          <h2 class="search">About Us</h2>
+             <div class="div2">
+             <h2>Introduction :</h2>
+             <p>My Name is Shuva </p>
+             </div>  
+          </div>
+    </header>
+    
+</body>
+</html>
